14901d07b8b4898d861818fc6f5dbb985709e81b
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-18 22:29:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  @@count = 0
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
 
-  def self.count
-    @@count
-  end
+
 
 
 
